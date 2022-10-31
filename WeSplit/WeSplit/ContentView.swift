c875e03aa314180b86a4b136479ec410996a456f
Andrew Cousin <86136052+andrewcousin@users.noreply.github.com>
2022-10-30 21:15:43 -0700

    Final Commit

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,21 +11,83 @@ struct ContentView: View {
 	@State private var checkAmount = 0.0
 	@State private var numberOfPeople = 2
 	@State private var tipPercentage = 20
+	@FocusState private var amountIsFocused: Bool
 	
-	let tipPercentages = [0, 5, 10, 15, 20, 25]
+	let tipPercentages = 0..<101
 	
+	var totalAmount: Double {
+		let tipSelection = Double(tipPercentage)
+		let tipvalue = checkAmount / 100 * tipSelection
+		let grandTotal = checkAmount + tipvalue
+		return grandTotal
+	}
 	
-    var body: some View {
-        Form {
-			Section {
-				TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+	var totalPerPerson: Double {
+		let peopleCount = Double(numberOfPeople + 2)
+		let tipSelection = Double(tipPercentage)
+		
+		let tipvalue = checkAmount / 100 * tipSelection
+		let grandTotal = checkAmount + tipvalue
+		let amountPerPerson = grandTotal / peopleCount
+		return amountPerPerson
+	}
+	
+	var localCurrency: FloatingPointFormatStyle<Double>.Currency {
+		(.currency(code: Locale.current.currencyCode ?? "USD"))
+	}
+	
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Amount", value: $checkAmount, format: localCurrency)
+						.keyboardType(.decimalPad)
+						.focused($amountIsFocused)
+					
+					Picker("Number of people", selection: $numberOfPeople) {
+						ForEach(2..<100) {
+							Text("\($0) people")
+						}
+					}
+					Section {
+						Picker("Tip percentage", selection: $tipPercentage) {
+							ForEach(tipPercentages, id: \.self) {
+								Text($0, format: .percent)
+							}
+						}
+						.pickerStyle(.wheel)
+					} header: {
+						Text("What % tip do you want to leave?")
+					}
+				}
+				Section {
+					Text(totalAmount, format: localCurrency)
+				} header: {
+					Text("Total Amount")
+				}
+				Section {
+					Text(totalPerPerson, format: localCurrency)
+				} header: {
+					Text("Amount per person")
+				}
+				
 			}
-        }
-    }
+			.navigationTitle("WeSplit")
+			.toolbar {
+				ToolbarItemGroup(placement: .keyboard) {
+					Spacer()
+					Button("Done") {
+						amountIsFocused = false
+					}
+				}
+			}
+			.navigationTitle("Amount per person")
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
