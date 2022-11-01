83f0fc7e4cea2c1bdaa0f2a8854614521e9d06c1
Andrew Cousin <86136052+andrewcousin@users.noreply.github.com>
2022-10-31 20:53:58 -0700

    1st commit for finish

---
+++ Unit Conversions/Unit Conversions/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Unit Conversions
+//
+//  Created by Andrew Cousin on 10/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State private var valueToConvert = 0.0
+	@State private var convertedValue = 0.0
+	@State private var unitArray = ["meters", "Km", "feet", "yards", "Miles"]
+	@State private var inputSelected = ""
+	@State private var outputSelected = ""
+	@FocusState private var valueIsFocused: Bool
+	
+	var convertToFeet: Double {
+		let feetConversion = ["meters": 3.28, "Km": 3_280.84, "feet": 1.00, "yards": 3.00, "Miles": 5_280.00]
+
+		if let x = feetConversion[inputSelected] {
+			if let y = feetConversion[outputSelected] {
+				return(x * valueToConvert / y)
+			}
+		}
+		return 0
+	}
+	
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Enter amount", value: $valueToConvert, format: .number)
+						.keyboardType(.decimalPad)
+						.focused($valueIsFocused)
+				}
+				Section {
+					Picker("Convert from:", selection: $inputSelected) {
+						ForEach(unitArray, id: \.self) {
+							Text($0)
+						}
+					}
+					.pickerStyle(.segmented)
+				} header: {
+					Text("Pick a unit to convert from")
+				}
+				Section {
+					Picker("Convert to:", selection: $outputSelected) {
+						ForEach(unitArray, id: \.self) {
+							Text($0)
+						}
+					}
+					.pickerStyle(.segmented)
+				} header: {
+					Text("Pick a unit to convert to")
+				}
+				Section {
+					Text(convertToFeet, format: .number)
+				} header: {
+					Text("Converted value")
+				}
+			}
+			.navigationTitle("Distance Conversions")
+			.toolbar {
+				ToolbarItemGroup(placement: .keyboard) {
+					Spacer()
+					Button("Done") {
+						valueIsFocused = false
+					}
+				}
+			}
+		}
+	}
+	
+	
+	struct ContentView_Previews: PreviewProvider {
+		static var previews: some View {
+			ContentView()
+		}
+	}
+}
---
+++ Unit Conversions/Unit Conversions/Unit_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionsApp.swift
+//  Unit Conversions
+//
+//  Created by Andrew Cousin on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
