100f2f6ae66ac9d43a011b6ec94892237fe9fbac
Andrew Cousin <86136052+andrewcousin@users.noreply.github.com>
2022-11-11 00:27:55 -0800

    Initial Commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andrew Cousin on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
